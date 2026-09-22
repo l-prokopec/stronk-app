14fe16ed0a62de968a62326c249dddc00ce44238
l-prokopec <koki.prokopec@gmail.com>
2026-09-22 17:16:55 +0200

    fix people configuration integration

--- src/storage/appStorage.ts
+++ src/storage/appStorage.ts
@@ -4,7 +4,7 @@ import { createId } from '../utils/id'
 export const STORAGE_KEY = 'gym-workout-tracker:v1'
 export const CORRUPT_STORAGE_PREFIX = `${STORAGE_KEY}:corrupt:`
 export const MIGRATION_BACKUP_PREFIX = `${STORAGE_KEY}:backup:v1:`
-const DEFAULT_NAMES = ['Leg press', 'Předkopávání', 'Zakopávání', 'Stahování horní kladky', 'Přítahy spodní kladky', 'Tlaky na prsa', 'Tlaky na ramena', 'Bicepsový zdvih', 'Triceps na kladce']
+const DEFAULT_NAMES = ['Kliky', 'Dead bug', 'Boční plank', 'Plank', 'Bulhaři', 'Rumuni', 'Předkopávání', 'Zakopávání', 'Leg press', 'Výpony na lýtka', 'Asistované shyby', 'Asistované dipy']
 const DEFAULT_PEOPLE: PersonProfile[] = [{ id: 'lukas', name: 'Lukáš', color: '#63cdbb' }, { id: 'terka', name: 'Terka', color: '#f08aaa' }]
 
 export const createInitialState = (): AppState => {
